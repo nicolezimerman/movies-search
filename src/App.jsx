fc9621cc1b2100d9c6fc8074ee56055fba73c6e8
nzimerman <nicole.zimerman@gmail.com>
2023-06-30 11:01:36 +0300

    Create project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <h1>Movies search</h1>
+      <form>
+        <input type="text" />
+        <button>Search</button>
+      </form>
+    </main>
+  );
+}
+
+export default App;
